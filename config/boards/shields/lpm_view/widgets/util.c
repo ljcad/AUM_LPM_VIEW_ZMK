a9bfac01debccdf0131ff3202605e6c7541d6349
ljcad <lj.feng@hotmail.com>
2026-02-11 14:51:03 +0800

    Update util.c

--- config/boards/shields/lpm_view/widgets/util.c
+++ config/boards/shields/lpm_view/widgets/util.c
@@ -15,62 +15,61 @@ void rotate_canvas(lv_obj_t *canvas, lv_color_t cbuf[]) {
     memcpy(cbuf_tmp, cbuf, sizeof(cbuf_tmp));
     lv_img_dsc_t img;
     img.data = (void *)cbuf_tmp;
-    img.header.format = LV_IMAGE_FORMAT_TRUE_COLOR;  // LVGL 9.x 标准宏
+    img.header.cf = LV_IMG_CF_TRUE_COLOR;  // LVGL 8.x 标准宏
     img.header.w = CANVAS_SIZE;
     img.header.h = CANVAS_SIZE;
 
     lv_canvas_fill_bg(canvas, LVGL_BACKGROUND, LV_OPA_COVER);
-    // LVGL 9.x 调整transform参数（角度单位为度，而非1/10度）
-    lv_canvas_transform(canvas, &img, -90, LV_IMG_ZOOM_NONE, 0, 0, CANVAS_SIZE / 2,
+    // LVGL 8.x 角度单位是1/10度，所以-900代表-90度
+    lv_canvas_transform(canvas, &img, -900, LV_IMG_ZOOM_NONE, 0, 0, CANVAS_SIZE / 2,
                         CANVAS_SIZE / 2 - 1, false);
 }
 
 void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
-    // LVGL 9.x 使用lv_draw_rect_params_t替代lv_draw_rect_dsc_t
-    lv_draw_rect_params_t rect_black_params;
-    lv_draw_rect_params_init(&rect_black_params);
-    rect_black_params.bg_color = LVGL_BACKGROUND;
-    
-    lv_draw_rect_params_t rect_white_params;
-    lv_draw_rect_params_init(&rect_white_params);
-    rect_white_params.bg_color = LVGL_FOREGROUND;
+    // LVGL 8.x 使用_dsc后缀的结构体
+    lv_draw_rect_dsc_t rect_black_dsc;
+    lv_draw_rect_dsc_t rect_white_dsc;
 
-    // LVGL 9.x 使用lv_canvas_draw_rect_params替代lv_canvas_draw_rect
-    lv_canvas_draw_rect_params(canvas, 0, 2, 29, 12, &rect_white_params);
-    lv_canvas_draw_rect_params(canvas, 1, 3, 27, 10, &rect_black_params);
-    lv_canvas_draw_rect_params(canvas, 2, 4, (state->battery + 2) / 4, 8, &rect_white_params);
-    lv_canvas_draw_rect_params(canvas, 30, 5, 3, 6, &rect_white_params);
-    lv_canvas_draw_rect_params(canvas, 31, 6, 1, 4, &rect_black_params);
+    // LVGL 8.x 初始化函数
+    init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
+    init_rect_dsc(&rect_white_dsc, LVGL_FOREGROUND);
+
+    // LVGL 8.x 原生绘制函数
+    lv_canvas_draw_rect(canvas, 0, 2, 29, 12, &rect_white_dsc);
+    lv_canvas_draw_rect(canvas, 1, 3, 27, 10, &rect_black_dsc);
+    lv_canvas_draw_rect(canvas, 2, 4, (state->battery + 2) / 4, 8, &rect_white_dsc);
+    lv_canvas_draw_rect(canvas, 30, 5, 3, 6, &rect_white_dsc);
+    lv_canvas_draw_rect(canvas, 31, 6, 1, 4, &rect_black_dsc);
 
     if (state->charging) {
-        lv_draw_img_params_t img_params;
-        lv_draw_img_params_init(&img_params);
-        lv_canvas_draw_img(canvas, 9, -1, &bolt, &img_params);
+        lv_draw_img_dsc_t img_dsc;
+        lv_draw_img_dsc_init(&img_dsc);
+        lv_canvas_draw_img(canvas, 9, -1, &bolt, &img_dsc);
     }
 }
 
-// LVGL 9.x 适配：参数结构体名从_dsc改为_params
-void init_label_dsc(lv_draw_label_params_t *label_params, lv_color_t color, const lv_font_t *font,
+// LVGL 8.x 原生函数声明（_dsc后缀）
+void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font,
                     lv_text_align_t align) {
-    lv_draw_label_params_init(label_params);
-    label_params->color = color;
-    label_params->font = font;
-    label_params->align = align;
+    lv_draw_label_dsc_init(label_dsc);
+    label_dsc->color = color;
+    label_dsc->font = font;
+    label_dsc->align = align;
 }
 
-void init_rect_dsc(lv_draw_rect_params_t *rect_params, lv_color_t bg_color) {
-    lv_draw_rect_params_init(rect_params);
-    rect_params->bg_color = bg_color;
+void init_rect_dsc(lv_draw_rect_dsc_t *rect_dsc, lv_color_t bg_color) {
+    lv_draw_rect_dsc_init(rect_dsc);
+    rect_dsc->bg_color = bg_color;
 }
 
-void init_line_dsc(lv_draw_line_params_t *line_params, lv_color_t color, uint8_t width) {
-    lv_draw_line_params_init(line_params);
-    line_params->color = color;
-    line_params->width = width;
+void init_line_dsc(lv_draw_line_dsc_t *line_dsc, lv_color_t color, uint8_t width) {
+    lv_draw_line_dsc_init(line_dsc);
+    line_dsc->color = color;
+    line_dsc->width = width;
 }
 
-void init_arc_dsc(lv_draw_arc_params_t *arc_params, lv_color_t color, uint8_t width) {
-    lv_draw_arc_params_init(arc_params);
-    arc_params->color = color;
-    arc_params->width = width;
+void init_arc_dsc(lv_draw_arc_dsc_t *arc_dsc, lv_color_t color, uint8_t width) {
+    lv_draw_arc_dsc_init(arc_dsc);
+    arc_dsc->color = color;
+    arc_dsc->width = width;
 }
