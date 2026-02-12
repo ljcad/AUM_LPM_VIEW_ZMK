78763188aa6a2019bb5e4af2c5e5fa930d23ace2
ljcad <lj.feng@hotmail.com>
2026-02-12 10:29:34 +0800

    Update util.h

--- config/boards/shields/lpm_view/widgets/util.h
+++ config/boards/shields/lpm_view/widgets/util.h
@@ -8,6 +8,8 @@
 #include <lvgl.h>
 #include <zmk/endpoints.h>
 
+#define NICEVIEW_PROFILE_COUNT 5
+
 #define CANVAS_SIZE 72
 #define CANVAS_COLOR_FORMAT LV_COLOR_FORMAT_L8 // smallest type supported by sw_rotate
 #define CANVAS_BUF_SIZE                                                                            \
@@ -27,6 +29,8 @@ struct status_state {
     int active_profile_index;
     bool active_profile_connected;
     bool active_profile_bonded;
+    bool profiles_connected[NICEVIEW_PROFILE_COUNT];
+    bool profiles_bonded[NICEVIEW_PROFILE_COUNT];
     uint8_t layer_index;
     const char *layer_label;
     uint8_t wpm[10];
