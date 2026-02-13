01ad88d3b5b4272cd472c9fd9cfc579b98e88fed
ljcad <lj.feng@hotmail.com>
2026-02-13 23:21:44 +0800

    Update peripheral_status.h

--- config/boards/shields/lpm_view/widgets/peripheral_status.h
+++ config/boards/shields/lpm_view/widgets/peripheral_status.h
@@ -4,29 +4,17 @@
  * SPDX-License-Identifier: MIT
  *
  */
+
 #pragma once
 
 #include <lvgl.h>
 #include <zephyr/kernel.h>
-#include <zmk/endpoints.h> // 必须包含，因为用到了 zmk_endpoint_instance 结构体
 #include "util.h"
-
-// 补全缺失的结构体定义
-struct status_state {
-    uint8_t battery;
-    bool charging;
-    struct zmk_endpoint_instance selected_endpoint;
-    uint8_t active_profile_index;
-    uint8_t layer_index;
-    const char *layer_label;
-};
 
 struct zmk_widget_status {
     sys_snode_t node;
     lv_obj_t *obj;
-    uint8_t cbuf[CANVAS_BUF_SIZE];
-    uint8_t cbuf2[CANVAS_BUF_SIZE];
-    uint8_t cbuf3[CANVAS_BUF_SIZE];
+    lv_color_t cbuf[CANVAS_SIZE * CANVAS_SIZE];
     struct status_state state;
 };
 
